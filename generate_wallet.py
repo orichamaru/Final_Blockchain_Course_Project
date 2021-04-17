4151b120628533fc13e0b6f51ca340269f09fbb0
orichamaru <jvdali966@gmail.com>
2021-04-17 10:57:51 +0530

    Naam le raha hai

--- generate_wallet.py
+++ generate_wallet.py
@@ -1,73 +1,55 @@
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Signature import PKCS1_v1_5
-from Crypto.Hash import SHA512, SHA384, SHA256, SHA, MD5
 from Crypto import Random
-from base64 import b64encode, b64decode
 import binascii
 
+
 class Wallet:
-     
-     def __init__(self,name=''):
-         self.public_key = ''
-         self.private_key = ''
-         self.name = name
-     
-     def generate_keys(self):
-          random_generator = Random.new().read
-          key = RSA.generate(1024, random_generator)
-          private, public = key, key.publickey()
-          
-          self.private_key = binascii.hexlify(private.exportKey(format='DER')).decode('ascii')
-          self.public_key = binascii.hexlify(public.exportKey(format='DER')).decode('ascii')
 
-          return self.private_key, self.public_key
+    def __init__(self, name=''):
+        self.public_key = ''
+        self.private_key = ''
+        self.name = name
 
+    def generate_keys(self):
+        random_generator = Random.new().read
+        key = RSA.generate(1024, random_generator)
+        private, public = key, key.publickey()
 
-     def get_keys(self):
-         
-         private_key,public_key = self.generate_keys()
+        self.private_key = binascii.hexlify(
+            private.exportKey(format='DER')).decode('ascii')
+        self.public_key = binascii.hexlify(
+            public.exportKey(format='DER')).decode('ascii')
 
-         private = open(self.name + "_private.txt","w")
-         public = open(self.name + "_public.txt","w")
+        return self.private_key, self.public_key
 
-         private.write(private_key)
-         private.close()
+    def get_keys(self):
 
-         public.write(public_key)
-         public.close()         
+        private_key, public_key = self.generate_keys()
 
+        private = open(self.name + "_private.txt", "w")
+        public = open(self.name + "_public.txt", "w")
+
+        private.write(private_key)
+        private.close()
+
+        public.write(public_key)
+        public.close()
+
+    def import_key(self):
+
+        private = open(self.name + "_private.txt", "r")
+        self.private_key = private.readline()
+
+        public = open(self.name + "_public.txt", "r")
+        self.public_key = public.readline()
     
-     def import_key(self):
-          
-         private = open(self.name + "_private.txt","r")
-         self.private_key = private.readline()
+    def generate_recipient_key(self,name):
+        
+        public_key = open(name + "_public.txt", "r")
+        recipient_public_key = public_key.readline()
+        
+        return recipient_public_key
 
-         public = open(self.name + "_public.txt","r")
-         self.public_key = public.readline()
-
-         
-         
-        #    private_key = RSA.importKey(fileContent) 
-        #  self.private_key = RSA.importKey(file.readLines())
-
-        #  public = self.name + "_public.pem"
-        #  self.public_key = RSA.importKey(open(public, "rb").decode('UTF-8'))
-         
-
-        #  print('\n\n',self.private_key.exportKey())
-
-# message = b'Hello'
-# cipher = PKCS1_OAEP.new(public)
-# print(b64encode(cipher.encrypt(message)))
-
-
-
-# random_generator = Random.new().read
-# key = RSA.generate(1024, random_generator)
-# pub_key = key.publickey()
-
-# self.public_key = binascii.hexlify(key.exportKey(format='DER')).decode('ascii')
-# self.private_key = binascii.hexlify(pub_key.exportKey(format='DER')).decode('ascii')
-
-# return self.public_key,self.private_key
+if __name__ == "__main__":
+    name = input("Enter Your name\n")
+    Wallet(name).get_keys()
--- transaction.py
+++ transaction.py
@@ -1,48 +1,57 @@
 from collections import OrderedDict
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Signature import PKCS1_v1_5
-from Crypto.Hash import SHA512, SHA384, SHA256, SHA, MD5
-from Crypto import Random
-from base64 import b64encode, b64decode
+from Crypto.Hash import SHA
 import binascii
-from blockchain_network import Blockchain
+import time
 
 
+# this class required to work on to make transactions more significant
 class Transaction:
 
-    def __init__(self,sender_public_key=None,sender_private_key=None,amount=0,recipient_public_key=None):
-        self.sender_public_key = sender_public_key
-        self.sender_private_key = sender_private_key
+    def __init__(self,  amount=0, sender_public_key=None, recipient_public_key=None, timestamp=time.time()):
         self.amount = amount
+        self.sender_public_key = sender_public_key
         self.recipient_public_key = recipient_public_key
-
+        self.timestamp = timestamp
+        self.signautre = None
 
     def get_transaction_bill(self):
-        return OrderedDict({'sender_public_key':self.sender_public_key,
-                            'recipient_public_key':self.recipient_public_key,
-                            'amount':self.amount})
+        return OrderedDict({'sender_public_key': self.sender_public_key,
+                            'recipient_public_key': self.recipient_public_key,
+                            'amount': self.amount,
+                            'timestamp': self.timestamp
+                            })
 
+    def sign_transaction(self, sender_private_key):
 
-    def sign_transactions(self):
-       
-        private_key = RSA.importKey(binascii.unhexlify(self.sender_private_key))
+        private_key = RSA.importKey(
+            binascii.unhexlify(sender_private_key))
         signer = PKCS1_v1_5.new(private_key)
         message = SHA.new(str(self.get_transaction_bill()).encode('utf8'))
-        signature = binascii.hexlify(signer.sign(message)).decode('ascii')
-        
-        return signature
+        self.signature = binascii.hexlify(signer.sign(message)).decode('ascii')
 
-    def generate_transaction(self):
-        
-        if( self.sender_public_key != None 
-            and self.sender_private_key !=None
-            and self.amount !=0
-            and self.recipient_public_key != None):
+    def verifyIt(self, blockchain):
 
-            digital_signature = self.sign_transactions()
-            a = Blockchain()
-            response = a.add_transaction(self.get_transaction_bill(),digital_signature)
-            
+        if(self.sender_public_key != None
+                and self.signature != None
+                and self.amount > 0
+                and self.recipient_public_key != None):
+
+            response = blockchain.verify_transaction(
+                self.get_transaction_bill(), self.signature)
+
             return response
-         
+        else:
+            return False
+
+    @staticmethod
+    def printIt(transaction):
+        if isinstance(transaction, Transaction):
+            transaction = transaction.get_transaction_bill()
+
+        print('amount : ', transaction['amount'])
+        print('sender : ', transaction['sender_public_key'][-50:])
+        print('reciever : ', transaction['recipient_public_key'][-50:])
+        print('timestamp : ', transaction['timestamp'])
+        print()
