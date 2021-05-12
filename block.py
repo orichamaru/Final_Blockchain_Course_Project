7aa050cfca14895979e2f02c58c74de0f020f1ab
ak0005 <akhilshukla2000@gmail.com>
2021-05-12 14:37:01 +0530

    added linearly variable difficulty

--- block.py
+++ block.py
@@ -1,16 +1,22 @@
-
 from transaction import Transaction
 
 
 class Block:
-
-    def __init__(self, minerId=-1, index=0, previous_hash=None, timestamp=1618473245.5043766, transactions=[], nonce=0):
+    def __init__(self,
+                 difficulty,
+                 minerId=-1,
+                 index=0,
+                 previous_hash=None,
+                 timestamp=1618473245.5043766,
+                 nonce=0):
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = timestamp
         self.transactions = []
         self.nonce = nonce
         self.minerId = minerId
+        self.minedAt = timestamp
+        self.difficulty = difficulty
 
     # Get block datagithub
     def get_block_data(self):
@@ -20,13 +26,15 @@ class Block:
             'timestamp': self.timestamp,
             'transactions': self.transactions,
             'nonce': self.nonce,
-            'minerId': self.minerId
+            'minerId': self.minerId,
+            'difficulty': self.difficulty
         }
         return data
 
     def printIt(self):
         print('index : ', self.index)
         print('miner id : ', self.minerId)
+        print('diificulty : ', self.difficulty)
         if self.previous_hash != None:
             print('previous_hash : ', self.previous_hash[-50:])
         print('timestamp : ', self.timestamp)
--- blockchain_network.py
+++ blockchain_network.py
@@ -5,8 +5,10 @@ import binascii
 import hashlib
 import json
 from block import Block
+import time
 
 DIFFICULTY = 5
+time_limit = 10  # seconds
 
 
 class Blockchain:
@@ -22,7 +24,7 @@ class Blockchain:
 
     # Create Genesis Block
     def create_genesis_block(self):
-        genesis_block = Block()
+        genesis_block = Block(DIFFICULTY)
         self.chain.append(genesis_block)
 
     # Adding peer
@@ -69,18 +71,31 @@ class Blockchain:
 
     # Proof of work
     def proof_of_work(self, block):
+        last_block = self.last_block_chain()
+        difficulty = DIFFICULTY
+
+        if last_block.index != 0:
+            if last_block.minedAt - last_block.timestamp < time_limit:
+                difficulty = last_block.difficulty + 1
+            elif last_block.minedAt - last_block.timestamp > time_limit:
+                difficulty = last_block.difficulty - 1
+            else:
+                difficulty = last_block.difficulty
 
         # block - block which needs to be added
-
+        block.difficulty = difficulty
         block.nonce = 0
         while not self.is_block_valid(block) and self.isStopped == False:
             block.nonce += 1
 
+        if self.isStopped == False:
+            block.minedAt = time.time()
+
     # Calculate hash of block
     def calculate_hash_of_block(self, block):
 
-        string_format_block = json.dumps(
-            block.get_block_data(), sort_keys=True).encode()
+        string_format_block = json.dumps(block.get_block_data(),
+                                         sort_keys=True).encode()
         return hashlib.sha256(string_format_block).hexdigest()
 
     # Add transaction to blockchain network
@@ -93,13 +108,15 @@ class Blockchain:
         if digital_signature == self.COMMISSION:
             return True
 
-        if(self.verify_signature(sender_public_key, transaction, digital_signature)):
+        if (self.verify_signature(sender_public_key, transaction,
+                                  digital_signature)):
             return True
         else:
             return False
 
     # Verifying transaction singature
-    def verify_signature(self, sender_public_key, transaction, digital_signature):
+    def verify_signature(self, sender_public_key, transaction,
+                         digital_signature):
 
         public_key = RSA.importKey(binascii.unhexlify(sender_public_key))
         verifier = PKCS1_v1_5.new(public_key)
@@ -108,21 +125,21 @@ class Blockchain:
         return verifier.verify(message, binascii.unhexlify(digital_signature))
 
     # Checking validity of block to be added
-    def is_block_valid(self, block, difficulty=DIFFICULTY):
+    def is_block_valid(self, block):
 
         # Calculate hash of block items
 
         block_hash = self.calculate_hash_of_block(block)
 
-        return block_hash[:difficulty] == '0'*difficulty
+        return block_hash[:block.difficulty] == '0' * block.difficulty
 
     # Add block to blockchain network
     def add_block(self, block):
         self.chain.append(block)
 
         for t in block.transactions:
-            self.maxTransactionTimeStamp = max(
-                self.maxTransactionTimeStamp, t['timestamp'])
+            self.maxTransactionTimeStamp = max(self.maxTransactionTimeStamp,
+                                               t['timestamp'])
 
     # Mining of block
     def mine(self, transactions, minerId, timestamp):
@@ -131,9 +148,10 @@ class Blockchain:
         prev_hash = self.calculate_hash_of_block(prev_block)
 
         # Creating new block
-        new_block = Block(minerId)
+        new_block = Block(DIFFICULTY)
 
-        new_block.index = prev_block.index+1
+        new_block.minerId = minerId
+        new_block.index = prev_block.index + 1
         new_block.previous_hash = prev_hash
 
         # Coinbase transaction is also included
@@ -142,7 +160,7 @@ class Blockchain:
         new_block.transactions = transactions
         new_block.timestamp = timestamp
 
-        # Nounce is correctily calculated
+        # Nonce is correctily calculated
         self.proof_of_work(new_block)
 
         # This valid block will be added to block chain
